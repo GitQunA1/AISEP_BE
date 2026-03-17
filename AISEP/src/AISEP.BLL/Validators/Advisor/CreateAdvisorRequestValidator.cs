200cff46f35ee1c44091ea7b84dda7bdb6791911
nguyenhongquan <emtenquan012@gmail.com>
2026-03-17 21:21:01 +0700

    fix error build

--- AISEP/src/AISEP.BLL/Validators/Advisor/CreateAdvisorRequestValidator.cs
+++ AISEP/src/AISEP.BLL/Validators/Advisor/CreateAdvisorRequestValidator.cs
@@ -14,23 +14,23 @@ namespace AISEP.BLL.Validators.Advisor
         {
             RuleFor(x => x.HourlyRate)
                 .NotEmpty().WithMessage("Hourly rate is required.")
-                .GreaterThan(0m).WithMessage("Hourly rate must be greater than 0.")
-                //.When(x => x.HourlyRate is not null && x.HourlyRate != 0);
+                .GreaterThan(0m).WithMessage("Hourly rate must be greater than 0.");
+            //.When(x => x.HourlyRate is not null && x.HourlyRate != 0);
 
             RuleFor(x => x.ProfileImageFile)
                 .NotEmpty().WithMessage("Profile image file is required.")
                 .Must(f => f!.Length <= MaxImageSize)
                     .WithMessage("Profile image must not exceed 5MB.")
                 .Must(f => AllowedImageTypes.Contains(f!.ContentType))
-                    .WithMessage("Profile image only supports JPG, PNG, WEBP.")
-                //.When(x => x.ProfileImageFile is not null);
+                    .WithMessage("Profile image only supports JPG, PNG, WEBP.");
+            //.When(x => x.ProfileImageFile is not null);
 
             RuleFor(x => x.CertificationFile)
                 .NotEmpty().WithMessage("Certification file is required.")
                 .Must(f => f!.Length <= MaxDocSize)
                     .WithMessage("Certification file must not exceed 10MB.")
                 .Must(f => AllowedDocTypes.Contains(f!.ContentType))
-                    .WithMessage("Certification only supports PDF, JPG, PNG.")
+                    .WithMessage("Certification only supports PDF, JPG, PNG.");
                // .When(x => x.CertificationFile is not null);
 
             RuleFor(x => x.Bio)
