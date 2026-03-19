35258a412e502c4210a065760b6ce332ac4a7c68
nguyenhongquan <emtenquan012@gmail.com>
2026-03-20 01:06:50 +0700

    add file migration 2 field for Transactions

---
+++ AISEP/src/AISEP.DAL/Migrations/20260319180215_AddReferenceFieldsToTransaction.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AISEP.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReferenceFieldsToTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReferenceId",
+                table: "transactions",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReferenceType",
+                table: "transactions",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferenceId",
+                table: "transactions");
+
+            migrationBuilder.DropColumn(
+                name: "ReferenceType",
+                table: "transactions");
+        }
+    }
+}
