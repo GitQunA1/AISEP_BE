9ca83962e8594a594ce9eac7fb9a4d5a4c99db6d
NguyenTongThanhAn <nguyentongthanhan@gmail.com>
2026-04-03 22:59:11 +0700

    Add status into project response

--- AISEP/src/AISEP.BLL/Helpers/MappingProfile.cs
+++ AISEP/src/AISEP.BLL/Helpers/MappingProfile.cs
@@ -288,6 +288,8 @@ namespace AISEP.BLL.Helpers
                     opt => opt.MapFrom(src => src.DevelopmentStage != null ? src.DevelopmentStage.ToString() : null))
                 .ForMember(dest => dest.Industry,
                     opt => opt.MapFrom(src => src.Industry.ToString()))
+                .ForMember(dest => dest.Status,
+                    opt => opt.MapFrom(src => src.Status.ToString()))
                 .ForMember(dest => dest.StartupPotentialScore,
                     opt => opt.MapFrom(src => src.StartupAIAnalysis != null ? src.StartupAIAnalysis.PotentialScore : null))
                 .ForMember(dest => dest.FollowerCount,
