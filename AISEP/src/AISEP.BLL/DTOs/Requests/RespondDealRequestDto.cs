6bb93b8884d351aad5cea9a33810d1abb6d87629
nguyenhongquan <emtenquan012@gmail.com>
2026-04-29 22:32:50 +0700

    remove code from old flow investment

--- AISEP/src/AISEP.BLL/DTOs/Requests/RespondDealRequestDto.cs
+++
@@ -1,8 +0,0 @@
-namespace AISEP.BLL.DTOs.Requests
-{
-    public class RespondDealRequestDto
-    {
-        public bool? IsAccepted { get; set; }
-        public string? Reason { get; set; }
-    }
-}
