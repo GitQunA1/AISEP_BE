34bed5a7fd006ac905b64b07ea0501b02a8f806e
nguyenhongquan <emtenquan012@gmail.com>
2026-05-05 17:35:02 +0700

    getAll Project for Staff & Admin

--- AISEP/src/AISEP.API/Controllers/ProjectsController.cs
+++ AISEP/src/AISEP.API/Controllers/ProjectsController.cs
@@ -25,18 +25,18 @@ namespace AISEP.API.Controllers
             _blockchainService = blockchainService;
         }
 
-        //[HttpGet]
-        //[Authorize]
-        //public async Task<IActionResult> GetAll([FromQuery] SieveModel model)
-        //{
-        //    var result = await _projectService.GetAllProjectsAsync(model);
-        //    return Ok(ApiResponse<object>.SuccessResponse(result, "Success"));
-        //}
-
         [HttpGet("non-premium")] 
         public async Task<IActionResult> GetAllForNonPremium([FromQuery] SieveModel model)
         {
             var result = await _projectService.GetAllProjectsForNonPremiumAsync(model);
+            return Ok(ApiResponse<object>.SuccessResponse(result, "Success"));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Staff, Admin")]
+        public async Task<IActionResult> GetAll([FromQuery] SieveModel model)
+        {
+            var result = await _projectService.GetAllProjectsAsync(model);
             return Ok(ApiResponse<object>.SuccessResponse(result, "Success"));
         }
 
