4d17d4b8bd43fda58d4cfb72a393caba191faff4
NguyenTongThanhAn <nguyentongthanhan@gmail.com>
2026-05-06 20:42:31 +0700

    Add function for aadmin

---
+++ AISEP/src/AISEP.BLL/DTOs/Responses/PlatformOverviewResponse.cs
@@ -0,0 +1,10 @@
+namespace AISEP.BLL.DTOs.Responses
+{
+    public class PlatformOverviewResponse
+    {
+        public int ActiveStartupCount { get; set; }
+        public int ActiveInvestorCount { get; set; }
+        public int ProjectCount { get; set; }
+        public int CompletedBookingCount { get; set; }
+    }
+}
